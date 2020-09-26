4dcfa72bd21445de60dd2ae6e80b889f0171a297
JoshSmith247 <54862229+JoshSmith247@users.noreply.github.com>
2020-09-25 22:21:53 -0500

    Update server.js

--- File Download Server/server.js
+++ File Download Server/server.js
@@ -8,20 +8,20 @@ http.createServer(function (req, res) {
           fs.readFile(path, function(err, data) {
             res.writeHead(200, {'Content-Type': 'text/zip'});
             res.write(data);
-            return res.end();
+            res.end();
           });
         break;
       case '/playerlookup'://TO DO: Link to homepage.html
           fs.readFile(path, function(err, data) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
-            return res.end();
+            res.end();
           });
         break;
       default:
         res.writeHead(200, {'Content-Type': 'text/html'});
             res.write("Oops! Page not found.");
-            return res.end();
+            res.end();
         break;
     }
 }).listen(8080);
