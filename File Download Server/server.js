55acf50fd2938cfdc14a2e5419a0d30b052e2957
JoshSmith247 <54862229+JoshSmith247@users.noreply.github.com>
2020-09-25 22:06:41 -0500

    Update server.js

--- File Download Server/server.js
+++ File Download Server/server.js
@@ -1,9 +1,26 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('readme.md', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+  var path = url.parse(request.url).pathname;
+    switch (path) {
+      case '/texturepack.zip'://TO DO: Link to homepage.html
+          fs.readFile(path, function(err, data) {
+            res.writeHead(200, {'Content-Type': 'zip'});
+            res.write(data);
+            return res.end();
+          });
+        break;
+      case '/playerlookup'://TO DO: Link to homepage.html
+          fs.readFile(path, function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+          });
+        break;
+      default:
+        res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("Oops! Page not found.");
+            return res.end();
+        break;
+    }
 }).listen(8080);
